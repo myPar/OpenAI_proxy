4d4ee721cb1da8fc77db5017be61ee29862f3b81
coss_mo <cossmo@DushezComp.localdomain>
2025-06-04 19:55:49 +0700

    fixed config for 32b r1

--- open_ai_proxy_r1/start_slurm.sh
+++ open_ai_proxy_r1/start_slurm.sh
@@ -28,11 +28,11 @@ export HF_HOME="/userspace/bak2/hf"
 export HF_TOKEN=""
 export VLLM_NO_USAGE_STATS=1
 export VLLM_CACHE_ROOT="/userspace/bak2/vllm_cache"
-export CUDA_VISIBLE_DEVICES=0
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 
 # run vllm server:
-vllm serve deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B \
-    --enable-reasoning --reasoning-parser deepseek_r1 --api-key='token-abc123' --port 8001 &
+vllm serve deepseek-ai/DeepSeek-R1-Distill-Qwen-32B \
+    --enable-reasoning --reasoning-parser deepseek_r1 --tensor-parallel-size 8 --api-key='token-abc123' --port 8001 &
 # cache pid for killing in future:
 VLLM_PID=$!
 echo "vLLM PID: $VLLM_PID"
@@ -69,10 +69,8 @@ done
 
 cd /userspace/bak2/MERA_space/MERA
 export OPENAI_API_KEY="token-abc123" 
-export MERA_FOLDER="/userspace/bak2/mera_results/api-R1-1.5B"
-export MERA_MODEL_STRING="model=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B,num_concurrent=8,base_url=http://localhost:8000/v1/chat/completions,tokenizer_backend=huggingface,tokenizer=deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B,timeout=6000"
-export MERA_COMMON_SETUP="--model openai-chat-completions --batch_size=1 --predict_only --log_samples --seed 1234 --verbosity INFO --apply_chat_template"
+export MERA_FOLDER="/userspace/bak2/mera_results/api-R1-32B"
+export MERA_MODEL_STRING="model=deepseek-ai/DeepSeek-R1-Distill-Qwen-32B,num_concurrent=8,base_url=http://localhost:8000/v1/chat/completions,tokenizer_backend=huggingface,tokenizer=deepseek-ai/DeepSeek-R1-Distill-Qwen-32B,timeout=60000"
+export MERA_COMMON_SETUP="--model local-chat-completions --batch_size=auto --max_batch_size=128 --predict_only --log_samples --seed 1234 --verbosity DEBUG --apply_chat_template"
+export SYSTEM_PROMPT="Реши задачу по инструкции ниже. Не давай никаких объяснений и пояснений к своему ответу. Не пиши ничего лишнего. Пиши только то, что указано в инструкции. Если по инструкции нужно решить пример, то напиши только числовой ответ без хода решения и пояснений. Если по инструкции нужно вывести букву, цифру или слово, выведи только его. Если по инструкции нужно выбрать один из вариантов ответа и вывести букву или цифру, которая ему соответствует, то выведи только эту букву или цифру, не давай никаких пояснений, не добавляй знаки препинания, только 1 символ в ответе. Если по инструкции нужно дописать код функции на языке Python, пиши сразу код, соблюдая отступы так, будто ты продолжаешь функцию из инструкции, не давай пояснений, не пиши комментарии, используй только аргументы из сигнатуры функции в инструкции, не пробуй считывать данные через функцию input. Не извиняйся, не строй диалог. Выдавай только ответ и ничего больше."
 bash scripts/run_benchmark_gen.sh
-
-kill $VLLM_PID
-kill $PROXY_PID
